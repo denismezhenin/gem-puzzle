2ecfe4caf41effce86a446934ab73d70a5f9dce0
denismezhenin <dzianismiazhenin@gmail.com>
2022-10-25 11:29:15 +0300

    feat: impliment drag and drop functionality

--- gem-puzzle/src/index.js
+++ gem-puzzle/src/index.js
@@ -94,7 +94,6 @@ let stopwatch;
 let sec = 0
 let min = 0
 let fieldWidth = field.offsetWidth
-// console.log(fieldWidth)
 let blockSize = fieldWidth / side
 let matrixFlat = []
 
@@ -114,16 +113,12 @@ const createBlocks = () => {
 		div.style.width = `${blockSize}px`
 		div.style.height = `${blockSize}px`
 		blockArray = Array.from(document.querySelectorAll('.block'))
-		
-		// do {
 			blockArray = SortArray(blockArray)
-		// } while (checkSolvable(blockArray) != true)
 }
 }
 
 const SortArray = (arr) => {
 	arr = arr.sort(() => Math.random() - 0.5)
-	// checkSolvable(arr)
 	return arr
 }
 
@@ -142,7 +137,6 @@ const checkSolvable = (arr) => {
 		}
 	}
 	let zeroblockRow = Math.floor(zeroblockIndex / side)
-	// console.log(counter, zeroblockIndex, zeroblockRow)
 if(side % 2 != 0) {
 	if((counter) % 2 != 0) {
 		startNewGame()
@@ -153,17 +147,8 @@ if(side % 2 == 0) {
 		startNewGame()
 	}
 }
-
-	// if((counter + zeroblockRow) % 2 != 0) {
-	// 	startNewGame()
-	// }
-	
-	// return true
 }
-// const sortBlocks = () => {
-// 			blockArray = Array.from(document.querySelectorAll('.block'))
-// 		blockArray = blockArray.sort(() => Math.random() - 0.5)
-// }
+
 
 let matrix = [];
 const buildMatrix = () => {
@@ -173,8 +158,6 @@ const buildMatrix = () => {
 	}
 	return matrix
 } 
-// buildmatrix()
-// console.log(blockArray[0])
 
 function setBlocksPosition (matrixArray) {
 	for (let i=0 ; i < side; i++) {
@@ -205,7 +188,6 @@ const startTimer = () => {
 function startNewGame() {
 	matrix.length = 0
 	etalon.length = 0
-	// console.log(blockArray.length)
 	if (blockArray.length > 0) {
 		blockArray.forEach(element => {
 			element.remove()
@@ -219,6 +201,8 @@ function startNewGame() {
 	startTimer()
 	makeEtalonArray()
 	checkSolvable(matrix)
+	removeMoveClass()
+	addMoveClass()
 	sec = 0
  	min = 0
 	moves = 0
@@ -227,11 +211,7 @@ function startNewGame() {
 	seconds.innerHTML  = '00'
 }
 
-
-
 restartButton.addEventListener('click', startNewGame)
-
-
 
 //audio 
 let isMute = false
@@ -269,42 +249,30 @@ function getZero() {
 
 field.addEventListener('click', (e) => {
 	target = e.target
-	// console.log(target)
 	getTarget()
-	// console.log(`X :${X} x :${x} Y: ${Y}, y: ${y}`)
-	
-	// console.log(matrix)
 	if ((Math.abs(X-x) == 1) && Y ==y) {
 		playAudio()
-
-		// console.log(compareToEtalon())
-		// console.log(`X :${X} x :${x} Y: ${Y}, y: ${y}`)
 		e.target.style.left = `${X * blockSize}px`;
 		matrix[Y][X].style.left = `${x * blockSize}px`;
 		[matrix[Y][X], matrix[y][x]] = [matrix[y][x], matrix[Y][X]];
 		 X=x
-		// console.log(matrix)
-		// console.log(`X :${X} x :${x} Y: ${Y}, y: ${y}`)
 		updatemoves()
 		compareToEtalon()
-		// console.log(compareToEtalon())
+		removeMoveClass()
+		addMoveClass()
 	}
 	if ((Math.abs(Y-y) == 1) && X == x) {
 		playAudio()
-		
-		// console.log(compareToEtalon())
 		e.target.style.top = `${Y * blockSize}px`;
 		matrix[Y][X].style.top = `${y * blockSize}px`;
-		// console.log(matrix);
 		[matrix[Y][X], matrix[y][x]] = [matrix[y][x], matrix[Y][X]];
 		Y = y;
-		// console.log(matrix);
 		moves++
 		updatemoves()
 		compareToEtalon()
-		// console.log(compareToEtalon())
+		removeMoveClass()
+		addMoveClass()
 	}
-
 })
 
 function getTarget() {
@@ -313,7 +281,6 @@ function getTarget() {
 			if(matrix[i][j] == target ) {
 				x = j 
 				 y= i
-				//  console.log(x,y)
 			}
 }
 	}
@@ -323,30 +290,86 @@ const updatemoves = () => {
 	moves++
 	return movesIndicator.innerHTML = `Moves: ${moves}`
 }
-// console.log(matrix)
-// const addMoveClass = () => {
-// 	if (Y==0) {
-// 	matrix[Y+1][X].classList.add('down')
-// 	}
-// 	if (Y == side -1) {
-// 		matrix[Y-1][X].classList.add('up')
-// 	}
-// 	if (Y > 0 && Y < side - 1) {
-// 		matrix[Y-1][X].classList.add('down')
-// 		matrix[Y+1][X].classList.add('up')
-// 	}
-// 	if (X==0) {
-// 	matrix[Y][X+1].classList.add('left')
-// 	}
-// 	if (X == side -1) {
-// 		matrix[Y][X-1].classList.add('right')
-// 	}
-// 	if (X > 0 && X < side -1) {
-// 		matrix[Y][X+1].classList.add('left')
-// 		matrix[Y][X-1].classList.add('right')
-// 	}
-// 	}
-// addMoveClass()
+
+const addMoveClass = () => {
+	matrix[Y][X].id = "target"
+	matrix[Y][X].addEventListener('drop', drop_handler)
+	matrix[Y][X].addEventListener('dragover', dragover_handler)
+	if (Y==0) {
+	matrix[Y+1][X].draggable = true;
+	matrix[Y+1][X].addEventListener('dragstart', dragstart_handler)
+	}
+	if (Y == side -1) {
+		matrix[Y-1][X].draggable = true;
+		matrix[Y-1][X].addEventListener('dragstart', dragstart_handler)
+	}
+	if (Y > 0 && Y < side - 1) {
+		matrix[Y-1][X].draggable = true;
+		matrix[Y-1][X].addEventListener('dragstart', dragstart_handler)
+		matrix[Y+1][X].draggable = true;
+		matrix[Y+1][X].addEventListener('dragstart', dragstart_handler)
+	}
+	if (X==0) {
+	matrix[Y][X+1].draggable = true;
+	matrix[Y][X+1].addEventListener('dragstart', dragstart_handler)
+	}
+	if (X == side -1) {
+		matrix[Y][X-1].draggable = true;
+		matrix[Y][X-1].addEventListener('dragstart', dragstart_handler)
+	}
+	if (X > 0 && X < side -1) {
+		matrix[Y][X+1].draggable = true;
+		matrix[Y][X+1].addEventListener('dragstart', dragstart_handler)
+		matrix[Y][X-1].draggable = true;
+		matrix[Y][X-1].addEventListener('dragstart', dragstart_handler)
+	}
+	}
+function dragstart_handler(event) {
+		target = event.target
+		getTarget()
+		event.dataTransfer.effectAllowed = "move";
+}
+function dragover_handler(event) {
+		event.preventDefault();
+		event.dataTransfer.dropEffect = "move"
+}
+function drop_handler(event) {
+		
+		event.preventDefault();	
+		if ((Math.abs(X-x) == 1) && Y ==y) {
+			playAudio()
+			target.style.left = `${X * blockSize}px`;
+			matrix[Y][X].style.left = `${x * blockSize}px`;
+			[matrix[Y][X], matrix[y][x]] = [matrix[y][x], matrix[Y][X]];
+			 X=x
+			updatemoves()
+			compareToEtalon()
+			removeMoveClass()
+			addMoveClass()
+		}
+		if ((Math.abs(Y-y) == 1) && X == x) {
+			playAudio()
+			target.style.top = `${Y * blockSize}px`;
+			matrix[Y][X].style.top = `${y * blockSize}px`;
+			[matrix[Y][X], matrix[y][x]] = [matrix[y][x], matrix[Y][X]];
+			Y = y;
+			moves++
+			updatemoves()
+			compareToEtalon()
+			removeMoveClass()
+			addMoveClass()
+		}
+}
+
+const removeMoveClass = () => {
+		let tempArr = matrix.flat()
+		for(let i = 0; i <tempArr.length; i++) {
+			tempArr[i].draggable = false;
+			tempArr[i].removeEventListener('dragstart', dragstart_handler);
+			tempArr[i].removeEventListener('dragover', dragover_handler);
+			tempArr[i].removeEventListener('drop', drop_handler);
+		}
+}
 
 // win
 
@@ -360,23 +383,11 @@ const makeEtalonArray = () => {
 }
 
 function compareToEtalon() {
-	// console.log(matrix)
-	// let isWin
 	matrixFlat = matrix.flat()
-	// matrixFlat = matrixFlat.flat()
-
-	// console.log(matrix)
-	// console.log(matrixFlat)
-	// console.log(etalon)
 	for(let i = 0; i < etalon.length; i++) {
-		// console.log(i)
-					// console.log(etalon[i] , ':', matrixFlat[i].innerHTML )
-			// console.log(matrixFlat[i].innerHTML)
 		if (etalon[i] != matrixFlat[i].innerHTML) {
-
 			return false
 		}
-
 }
 showWin()
 setRecord()
@@ -400,15 +411,11 @@ function setRecord() {
 		resultArray = JSON.parse(localStorage.getItem('results'))
 	}
 	if(resultArray.length > 9) resultArray.length = 9;
-	// resultArray.length = 9
 	resultArray.push(temp)
 	resultArray.sort(function(a, b) {
 		return a.moves-b.moves
 	})
-
 	localStorage.setItem('results', JSON.stringify(resultArray));
-	// console.log(JSON.parse(localStorage.getItem('results')))
-
 }
 
 winWrapper.addEventListener('click', () => {
@@ -424,7 +431,6 @@ function showRecords() {
 	if(localStorage.getItem('results')) {
 		resultArray = JSON.parse(localStorage.getItem('results'))
 	}
-	// console.log(resultArray.length)
 	if (resultArray.length == 0) {
 		let p = document.createElement('p')
 		p.innerHTML = "No records yet"
@@ -436,7 +442,6 @@ function showRecords() {
 			recordsWrapper.append(p)
 			}
 	}
-	
 }
 
 recordsWrapper.addEventListener('click', () => {
@@ -449,17 +454,12 @@ recordsWrapper.addEventListener('click', () => {
 // easy mode 
 easyModeArray = []
 const easyMode = () => {
-	// console.log(blockArray)
 	easyModeArray = blockArray.slice(0)
 	easyModeArray = easyModeArray.sort( function(a, b) {
 		return a.innerHTML - b.innerHTML
 	})
 	let temp = easyModeArray.splice(0, 1)
 	easyModeArray.splice(easyModeArray.length - 1, 0, temp[0])
-	// console.log(easyModeArray)
-	// console.log(temp)
-	// console.log(blockArray)
-
 	return easyModeArray
 }
 
@@ -472,7 +472,6 @@ easyModeButton.addEventListener('click', () => {
 			element.remove()
 		});
 	}
-	// easyMode()
 	createBlocks()
 	buildMatrix()
 	easyMode()
@@ -481,15 +480,14 @@ easyModeButton.addEventListener('click', () => {
 	startTimer()
 	startTimer()
 	makeEtalonArray()
+	removeMoveClass()
+	addMoveClass()
 	sec = 0
  	min = 0
 	moves = 0
 	movesIndicator.innerHTML = `Moves: ${moves}`
 	minutes.innerHTML  = '00'
 	seconds.innerHTML  = '00'
-	// console.log(blockArray)
-	// console.log(matrix.flat())
-	// console.log(etalon)
 })
 
 
@@ -499,8 +497,6 @@ saveButton.addEventListener('click',
 savegame)
 let savedArray = [];
 function savegame() {
-	// let saveArray = matrix.flat()
-	// console.log(saveArray)
 	let save = matrix.flat().map(el => el.innerHTML)
 	let saveObj = {
 		minutes:  `${minutes.innerHTML}`,
@@ -510,17 +506,9 @@ function savegame() {
 		blocksNumber: `${blocksNumber}`,
 		blockSize: `${blockSize}`,
 	}
-	// console.log(saveObj)
 
 	localStorage.setItem('lastSave', JSON.stringify(save));
 	localStorage.setItem('lastSaveIndicators', JSON.stringify(saveObj));
-	// localStorage.lastsave = JSON.stringify(dd)
-	// console.log(localStorage.setItem('lastSaveIndicators', JSON.stringify(saveObj)))
-
-
-
-	// let d = JSON.stringify(saveArray[0], Object.getOwnPropertyNames(target["__proto__"]));
-	// console.log(d)
 }
 
 function compareArrays(arr) {
@@ -555,18 +543,16 @@ if(localStorage.getItem('lastSave')) {
 	startTimer()
 	startTimer()
 	makeEtalonArray()
+	removeMoveClass()
+	addMoveClass()
 	sec = (JSON.parse(localStorage.getItem('lastSaveIndicators'))).seconds;
  	min = (JSON.parse(localStorage.getItem('lastSaveIndicators'))).minutes;
 	moves = (JSON.parse(localStorage.getItem('lastSaveIndicators'))).moves;
 	movesIndicator.innerHTML = `Moves: ${moves}`
 	minutes.innerHTML  = `${min}`
 	seconds.innerHTML  = `${sec}`
+	document.querySelectorAll('option').forEach(el => el.value == side? el.selected = true : el.selected = false)
 }
 }
 
-//initial shuffle
-// createBlocks()
-// buildMatrix()
-// setBlocksPosition(blockArray)
-// getZero()
 
